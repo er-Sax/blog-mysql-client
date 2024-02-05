53b47480e2856dc3890a403e6278e200f20b68a4
er-Sax <83650953+er-Sax@users.noreply.github.com>
2024-02-05 23:36:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,24 @@
-//import { defineConfig } from 'vite'
-//import react from '@vitejs/plugin-react'
-//import basicSsl from '@vitejs/plugin-basic-ssl'
-
-// https://vitejs.dev/config/
-//export default defineConfig({
-//  plugins: [react()],
-//  server: {
-//    host: true,
-//    port: process.env.PORT,
-//  },
-//  build: {
-//        rollupOptions: {
-//            output:{
-//                manualChunks(id) {
-//                    if (id.includes('node_modules')) {
-//                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-//                    }
-//               }
-//            }
-//        }
-//   }
-//})
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
-// https://vitejs.dev/config/
+ https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: process.env.PORT,
+  },
+  build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+               }
+            }
+        }
+   }
 })
+
