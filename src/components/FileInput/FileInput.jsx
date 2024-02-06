4e1f1cc134318dcfadcce739804fd640e543271f
er-Sax <83650953+er-Sax@users.noreply.github.com>
2024-02-06 23:23:37 +0100

    Add files via upload

--- src/components/FileInput/FileInput.jsx
+++ src/components/FileInput/FileInput.jsx
@@ -1,9 +1,10 @@
 import React, {useState, useRef} from 'react';
-import './fileinput.scss'
+import './fileinput.scss';
+import AlertPopup from '../AlertPopup/AlertPopup';
 
 const FileInput = ({file, setFile, state}) => {
    const [fileName, setFileName] = useState('');
-
+   const [openPopup, setOpenPopup] = useState(false);
    const fileInputRef = useRef(null);
 
    const handleFileUploadClick = () => {
@@ -11,10 +12,26 @@ const FileInput = ({file, setFile, state}) => {
          fileInputRef.current.click();
       }
    };
+
+   const handleAlertConfirm = () => {
+      setOpenPopup(false);
+   };
+
    const handleSetFile = () => {
       const selectedFile = fileInputRef.current.files[0];
-      setFile(selectedFile);
-      setFileName(selectedFile.name);
+      if (selectedFile) {
+         const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+         if (allowedTypes.includes(selectedFile.type)) {
+            setFile(selectedFile);
+            setFileName(selectedFile.name);
+         } else {
+            // alert('Please choose a valid .jpeg, .jpg, or .png file.');
+            setOpenPopup(true);
+            fileInputRef.current.value = null;
+            setFile(null);
+            setFileName('');
+         }
+      }
    };
 
    const handleClearFile = () => {
@@ -56,6 +73,12 @@ const FileInput = ({file, setFile, state}) => {
                </div>
             )}
          </div>
+         {openPopup && (
+            <AlertPopup
+               message='Please choose a valid .jpg, or .png file.'
+               onConfirm={handleAlertConfirm}
+               showCancel={false}></AlertPopup>
+         )}
       </div>
    );
 };
